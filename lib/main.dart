dbd610c6b02719eaf099b5a6fbd8c63c4314e10c
PedroSawczuk <pedrosawczuk53@gmail.com>
2024-09-17 20:34:09 -0400

    Adiciona autenticação ao projeto

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,20 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:paises_app/views/homePage.dart';
+import 'package:paises_app/firebase_options.dart';
+import 'package:paises_app/views/auth/signInPage.dart';
 
-void main() {
+Future<void> main() async {
+
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(
     GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
+      home: SignInPage(),
     ),
   );
 }
